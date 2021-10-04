d967f06c8ef90d8ca9ffcfc8b0d991ede779fe39
Ferdinando Di Palma <ferdinando.dipalma@stepsrl.org>
2021-10-04 12:55:08 +0200

    Init

---
+++ src/org/step/validazione/Main.java
@@ -0,0 +1,39 @@
+package org.step.validazione;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start");
+				
+		/*
+		 * 1 dati 3 valori a,b,c mostrare a video il contenuto della variabile più piccola
+		 * senza analizzare casi di uguaglianza
+		*/
+		
+		/*
+		 * 2 realizzare il punto 1 ma come funzione, con valore di ritorno void e tre parametri in input
+		 */
+		
+		/*
+		 * 3 realizzare il punto 1 ma come funzione, con valore di ritorno int, tre paramentri di input e stampando a video nel main
+		 */
+		
+		/*
+		 * 4 dato un array di 5 elementi stampare la media del primo ed ultimo elemento
+		 */
+		
+		/*
+		 * 5 dato un array di 6 elementi sommare solo i valori contenuti nella PRIMA metà dell array
+		 */
+		
+		/*
+		 * 6 dato un array di 6 elementi fare la media solo dei valori contenuti nella SECONDA metà dell array
+		 */
+		
+		/*
+		 * 7 Realizzare una funzione con input un array di interi che sia in grado si stampare a video i conteuto
+		 */
+	}
+
+}
---
+++ src/org/step/validazione/MainTest.java
@@ -0,0 +1,9 @@
+package org.step.validazione;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		System.out.println("asd");
+	}
+
+}
